1f91625c79e20a336a3fe5c50ac3fabc0516e252
fernando.junior <fernando.junior@informata.com.br>
2023-05-15 16:18:23 -0300

    :sparkles: sobe mudancas no app.js para menu sidebar

--- src/main/App.js
+++ src/main/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TelaPrincipal from '../components/pages/TelaPrincipal';
 import Cadastro from '../components/pages/Cadastro';
 import TabelaDespesas from '../components/pages/TabelaDespesas';
 import Footer from '../components/template/Footer';
 import Logo from '../components/template/Logo';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import SideBar from '../components/template/SideBar';
+import './App.css'; // Importe um arquivo CSS para estilização
 
 function App() {
+
+
   const fetchDespesas = () => {
     // lógica para buscar as despesas no banco de dados
-  }
+  };
 
   return (
     <Router>
-       <Logo />
-      <Routes>
-        <Route path="/" element={<TelaPrincipal />} />
-        <Route path="/cadastro/:id" element={<Cadastro fetchDespesas={fetchDespesas} />} />
-        <Route path="/cadastro" element={<Cadastro fetchDespesas={fetchDespesas} />} />
-        <Route path="/tabelaDespesas" element={<TabelaDespesas fetchDespesas={fetchDespesas} />} />
-      </Routes>
+      <Logo />
+      <div className="">
+        <SideBar></SideBar>
+        <main>
+          <Routes>
+            <Route path="/" element={<TelaPrincipal />} />
+            <Route path="/cadastro/:id" element={<Cadastro fetchDespesas={fetchDespesas} />} />
+            <Route path="/cadastro" element={<Cadastro fetchDespesas={fetchDespesas} />} />
+            <Route path="/tabelaDespesas" element={<TabelaDespesas fetchDespesas={fetchDespesas} />} />
+          </Routes>
+        </main>
+      </div>
       <Footer />
     </Router>
   );
